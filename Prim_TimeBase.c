e1b048edfc7b35baa4921c8414ca49d30fb15bd3
khangdt003 <khangdt003@gmail.com>
2025-10-01 16:56:06 +0700

    _Cnt_ to _Count_

--- Prim_TimeBase.c
+++ Prim_TimeBase.c
@@ -2,20 +2,21 @@
 #define COUNT_DOWN 0
 #define PRECHARGE_TIME 0.02 // seconds
 #define SAMPLING_FREQUENCY 100000 // 100 kHz
+
 //------------------------------------
-//------------- INPUTS -------------
+//---------- INPUTS -----------
 //------------------------------------
 long double CLOCK_CYCLE_FREQUENCY = 1 / delt;
 long double PWM_FREQUENCY = x1;
 // long double PWM_PHASE_SHIFT = x2; // Degree
 
 //------------------------------------
-//------- DEFINEATIONS ---------
+//------- DEFINITIONS ----------
 //------------------------------------
 long double PWM_TBPRD_MAX = (CLOCK_CYCLE_FREQUENCY / PWM_FREQUENCY / 2);
 long double PRECHARGE_MAX = (SAMPLING_FREQUENCY * PRECHARGE_TIME);
-static int PWM_Cnt_PrimLeg1 = 0, PWM_Cnt_PrimLeg2 = 0;
-static int PWM_Cnt_dir_PrimLeg1 = 0, PWM_Cnt_dir_PrimLeg2 = 0;
+static int PWM_Count_PrimLeg1 = 0, PWM_Count_PrimLeg2 = 0;
+static int PWM_Count_dir_PrimLeg1 = 0, PWM_Count_dir_PrimLeg2 = 0;
 static long PreCharge_Count = 0;
 static long Phase_PrimLeg = 0;
 static int Enable_PhaseShift = 0, Enable_PreCharge = 0;
@@ -24,9 +25,7 @@ static int ISR = 0;
 static int check = 0;
 ISR_COUNT_MAX = 1 / (SAMPLING_FREQUENCY * delt);
 
-//------------------------------------
 //-------- ISR HAPPEN ----------
-//------------------------------------
 ISR_Count++;
 if (ISR_Count > ISR_COUNT_MAX)
 {
@@ -49,39 +48,33 @@ if (ISR == 1)
 	}
 }
 
-//------------------------------------
 //--- PHASE SHIFT MODULE -----
-//------------------------------------
 if (Enable_PhaseShift == 1)
 {
-	if (PWM_Cnt_PrimLeg1 == Phase_PrimLeg)
+	if (PWM_Count_PrimLeg1 == Phase_PrimLeg)
 	{
-		PWM_Cnt_PrimLeg2 = 0;
+		PWM_Count_PrimLeg2 = 0;
 		Enable_PhaseShift = 0;
 	}
 }
 
-//------------------------------------
 //------ SAWTOOTH LEG1 -------
-//------------------------------------
-if (PWM_Cnt_PrimLeg1 >= PWM_TBPRD_MAX) PWM_Cnt_dir_PrimLeg1 = COUNT_DOWN;
-else if (PWM_Cnt_PrimLeg1 <= 0) PWM_Cnt_dir_PrimLeg1 = COUNT_UP;
-if (PWM_Cnt_dir_PrimLeg1 == COUNT_UP) PWM_Cnt_PrimLeg1++;
-if (PWM_Cnt_dir_PrimLeg1 == COUNT_DOWN) PWM_Cnt_PrimLeg1--;
+if (PWM_Count_PrimLeg1 >= PWM_TBPRD_MAX) PWM_Count_dir_PrimLeg1 = COUNT_DOWN;
+else if (PWM_Count_PrimLeg1 <= 0) PWM_Count_dir_PrimLeg1 = COUNT_UP;
+if (PWM_Count_dir_PrimLeg1 == COUNT_UP) PWM_Count_PrimLeg1++;
+if (PWM_Count_dir_PrimLeg1 == COUNT_DOWN) PWM_Count_PrimLeg1--;
 
-//------------------------------------
 //-------- SAWTOOTH LEG2 -----
-//------------------------------------
-if (PWM_Cnt_PrimLeg2 >= PWM_TBPRD_MAX) PWM_Cnt_dir_PrimLeg2 = COUNT_DOWN;
-else if (PWM_Cnt_PrimLeg2 <= 0) PWM_Cnt_dir_PrimLeg2 = COUNT_UP;
-if (PWM_Cnt_dir_PrimLeg2 == COUNT_UP) PWM_Cnt_PrimLeg2++;
-if (PWM_Cnt_dir_PrimLeg2 == COUNT_DOWN) PWM_Cnt_PrimLeg2--;
+if (PWM_Count_PrimLeg2 >= PWM_TBPRD_MAX) PWM_Count_dir_PrimLeg2 = COUNT_DOWN;
+else if (PWM_Count_PrimLeg2 <= 0) PWM_Count_dir_PrimLeg2 = COUNT_UP;
+if (PWM_Count_dir_PrimLeg2 == COUNT_UP) PWM_Count_PrimLeg2++;
+if (PWM_Count_dir_PrimLeg2 == COUNT_DOWN) PWM_Count_PrimLeg2--;
 
 //------------------------------------
 //---------- OUTPUTS -------------
 //------------------------------------
-y1 = (float)PWM_Cnt_PrimLeg1 / (float)PWM_TBPRD_MAX;
-y2 = (float)PWM_Cnt_PrimLeg2 / (float)PWM_TBPRD_MAX;
+y1 = (float)PWM_Count_PrimLeg1 / (float)PWM_TBPRD_MAX;
+y2 = (float)PWM_Count_PrimLeg2 / (float)PWM_TBPRD_MAX;
 y3 = Phase_PrimLeg;
 y4 = PreCharge_Count;
 y5 = PRECHARGE_MAX;
